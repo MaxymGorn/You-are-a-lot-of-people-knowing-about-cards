e047571ad1dfad0cca215b874a6d09f7b660b1a8
Max56132 <50423072+MaxymGorn@users.noreply.github.com>
2020-07-05 03:09:33 +0300

    коміт

---
+++ WindowsFormsApp30/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace WindowsFormsApp30
+{
+    public partial class Form1 : Form
+    {
+        Score Score;
+        public Form1()
+        {
+            Score = new Score();
+            InitializeComponent();
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Schet.Text = Score.FirstlyScore.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                Thread[] threads = new Thread[20];
+                Action<int> action = delegate (int iteration) { Score.UpdatePursh(iteration); };
+                for (int i = 0; i < 20; i++)
+                {
+                    var operation = new ParameterizedThreadStart(obj => action((int)numericUpDown1.Value));
+                    threads[i] = new Thread(operation);
+                    threads[i].Start();
+                }
+                Snato.Text = Score.CountScore.ToString();
+                Schet.Text = Score.FirstlyScore.ToString();
+
+        }
+    }
+}
---
+++ WindowsFormsApp30/Score.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp30
+{
+    class Score
+    {
+        int score=10000;
+        static object obj = new object();
+        public int FirstlyScore { get { return score; } }
+        public int CountScore { get { return 10000- score; } }
+
+        public void UpdatePursh(int iteration)
+        {
+            for (int i = 0; i < iteration; i++)
+            {
+                if (score > 0)
+                {
+                    //score--;
+                    Interlocked.Decrement(ref score);
+                }
+            }
+        }
+    }
+}
